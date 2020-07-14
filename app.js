ca9945a41ba36606177f63fae611921faa4d9656
Frankpeter Ani <frankpeterani@gmail.com>
2020-07-14 15:00:43 +0100

    Yeah

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
-gsap.from(".hero", { duration: 2, x: "-100vw", ease: "power2.out" });
-gsap.from(".coming-soon", {
+gsap.from(".hero, .hero-two", { duration: 2, x: "-100vw", ease: "power2.out" });
+gsap.from(".coming-soon, .coming-soon-two", {
   duration: 2,
   opacity: 0,
   delay: 3.5,
   ease: "bounce.in",
   stagger: 2,
 });
-gsap.from(".desc", { duration: 2, x: "100vw", delay: 2, ease: "power2.out" });
-gsap.from(".form", { duration: 2, x: "-100vw", delay: 2, ease: "power2.out" });
-gsap.from(".notify", {
+gsap.from(".desc, .desc-two", {
+  duration: 2,
+  x: "100vw",
+  delay: 2,
+  ease: "power2.out",
+});
+gsap.from(".form, .form-two", {
+  duration: 2,
+  x: "-100vw",
+  delay: 2,
+  ease: "power2.out",
+});
+gsap.from(".notify, .notify-two", {
   duration: 2,
   opacity: 0,
   delay: 2,
   ease: "bounce.in",
   stagger: 2,
 });
-gsap.from(".social-flex", {
+gsap.from(".social-flex-two", {
   duration: 2,
   x: "100vw",
   delay: 2,
   ease: "power4.out",
 });
-gsap.from(".socials a:nth-child(1)", {
+gsap.from(".socials a:nth-child(1), .socials-two a:nth-child(1)", {
   duration: 2,
   x: "-100vw",
   delay: 2,
   ease: "power4.out",
 });
-gsap.from(".top", {
+gsap.from(".top, .top-two", {
   duration: 1,
   y: "-500%",
   ease: "bounce",
